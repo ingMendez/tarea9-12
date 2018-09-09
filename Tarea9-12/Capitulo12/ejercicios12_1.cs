a8c6b0ad497e601c8589358caa7188bd9c9e0ca8
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-09-09 17:36:21 -0400

    progreso de tares#3

---
+++ Tarea9-12/Capitulo12/ejercicios12_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarea9_12.Capitulo12
+{
+   public  class ejercicios12_1
+    {
+        public ejercicios12_1()
+        {
+
+        }
+
+        public static int factorial()
+        {
+            int num = 0, res = 1;
+            Console.Write("Digite Numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 1)
+                return num;
+
+            for (int i = 1; i <= num; ++i)
+                res *= i;
+
+            Console.WriteLine("El Factorial de {0} es {1} ", num, res);
+            Console.WriteLine();
+            return num;
+        }
+    }
+}
---
+++ Tarea9-12/Capitulo12/ejercicios12_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarea9_12.Capitulo12
+{
+    public class ejercicios12_2
+    {
+        public ejercicios12_2()
+        {
+
+        }
+
+        public static void factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+                Console.WriteLine(fact);
+            }
+            Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+            Console.ReadKey();
+
+        }
+    }
+}
